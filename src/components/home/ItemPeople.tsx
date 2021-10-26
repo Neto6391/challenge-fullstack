974ce1e37f380fad5f0f3fa8c2eeec062a1e5869
nettorodrigues <neto6391@gmail.com>
2021-10-26 01:26:39 -0300

    feat(list-characters-sw): create a component item people

--- src/components/home/ItemPeople.tsx
+++ src/components/home/ItemPeople.tsx
@@ -1,19 +1,45 @@
 import { Card, CardActionArea, Typography } from "@mui/material";
+import { useState } from "react";
+import { People } from "../../interfaces/People";
+import ImageCharacterCard from "./ImageCharacter";
+import ModalToShowCharacter from "./ModalToShowCharacter";
 
-export interface People {
-    name: string
+export interface StyleProps extends People {
+    style?: Record<any, any>
 }
 
-function ItemPeople({ ...props }: People) {
+function ItemPeople({ ...props }: StyleProps) {
+    const [isModalOpen, setModalOpen] = useState(false);
+
+    function closeModal() {
+        setModalOpen(false);
+    }
+
+    const people: People = Object.assign({},{...props}, { style: null });
 
     return (
         <Card>
-            <CardActionArea onClick={() => console.log(`is clicked in ${props.name}`)}>
+            <CardActionArea style={props.style} onClick={() => setModalOpen(true)}>
+                <ImageCharacterCard name={props.name} heigth={"340"}  />
                 <Typography variant="body1" align="center" color="textPrimary">
                     { props.name }
                 </Typography>
             </CardActionArea>
+            { isModalOpen && <ModalToShowCharacter 
+                closeIconbuton={closeModal} 
+                open={isModalOpen} 
+                id={people.id} 
+                name={people.name} 
+                birth_year={people.birth_year} 
+                gender={people.gender} 
+                hair_color={people.hair_color} 
+                height={people.height} 
+                mass={people.mass} 
+                skin_color={people.skin_color} 
+                films={people.films} 
+            /> }
         </Card>
+        
     );
 }
 
