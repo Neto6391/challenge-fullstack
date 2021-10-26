fe68fc14a270c34291fa69d83fe9b94b54750ca4
nettorodrigues <neto6391@gmail.com>
2021-10-26 01:29:47 -0300

    feat(list-characters-sw): create a component to render image in extra api sw

---
+++ src/components/home/ImageCharacter.tsx
@@ -0,0 +1,23 @@
+import { useMemo } from "react";
+import { ImageCharacter } from "../../interfaces/ImageCharacter";
+import characterWithImage from "../../data/characterWithImage.json";
+import CardMedia from "@mui/material/CardMedia";
+
+
+function ImageCharacterCard({...props}: ImageCharacter) {
+
+    const getCharacterImageURL: any = useMemo(() => () => (characterWithImage.filter((character: any) => character.name === props.name )[0]), [props.name] );
+    
+    characterWithImage.filter((character: any) => character.name === props.name );
+
+    console.log(Object.prototype.isPrototypeOf.call(getCharacterImageURL(), "image"));
+    const characterImage: Record<string, unknown> = Object.assign({}, getCharacterImageURL());
+    return (
+        characterImage && characterImage.image ? (
+            <CardMedia 
+            component="img"
+            height={props.heigth}
+            image={getCharacterImageURL().image} />) : (<p></p>));
+}
+
+export default ImageCharacterCard;
