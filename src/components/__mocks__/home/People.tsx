49cf95e6efb528ac0bd91cd01e2f0446cfffc29b
nettorodrigues <neto6391@gmail.com>
2021-10-26 01:29:19 -0300

    feat(list-characters-sw): create a mocks

---
+++ src/components/__mocks__/home/People.tsx
@@ -0,0 +1,31 @@
+import { People } from "../../../interfaces/People";
+
+export const List: Array<People> = [
+    {
+        id: "item-people-0",
+        name: "teste 1",
+        birth_year:"19BBY",
+        gender: "male",
+        hair_color: "blond",
+        height: "172",
+        mass: "77",
+        skin_color: "fair", 
+        films:[
+            "https://swapi.dev/api/films/1/",
+            "https://swapi.dev/api/films/2/",
+        ] 
+    },
+    {
+        id: "item-people-1",
+        name: "teste 2",
+        birth_year:"19BBY",
+        gender: "male",
+        hair_color: "blond",
+        height: "172",
+        mass: "77",
+        skin_color: "fair", 
+        films:[
+            "https://swapi.dev/api/films/3/",
+        ] 
+    },
+]
