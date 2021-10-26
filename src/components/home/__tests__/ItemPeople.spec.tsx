cb6b418cd6c6f3d88a0efcb85d14d932049d0857
nettorodrigues <neto6391@gmail.com>
2021-10-26 01:26:58 -0300

    feat(list-characters-sw): create a test to component item people

--- src/components/home/__tests__/ItemPeople.spec.tsx
+++ src/components/home/__tests__/ItemPeople.spec.tsx
@@ -1,10 +1,15 @@
 import { render } from '@testing-library/react';
+import { List } from '../../__mocks__/home/People';
 import ItemPeople from '../ItemPeople';
 
+const props = {
+    people: List[0],
+}
+
 describe('<ItemPeople />', () => {
-    it('render correctly', () => {
-        const {getByText, asFragment} = render(<ItemPeople name="teste1" />);
-        expect(getByText(/teste1/i)).toBeInTheDocument();
+    it('item render correctly', () => {
+        const {getByText, asFragment} = render(<ItemPeople {...props.people} />);
+        expect(getByText(/teste 1/i)).toBeInTheDocument();
       });
 });
 
